2c46b7c7f12bce68b167ee9c8364a5735d6f062d
sivani-r-anil <sivaniranil28@gmail.com>
2026-07-09 15:23:58 +0530

    search book is added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddBook from './components/AddBook'
+import SearchBook from './components/SearchBook'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <AddBook />
+      <SearchBook />
     </>
   )
 }
